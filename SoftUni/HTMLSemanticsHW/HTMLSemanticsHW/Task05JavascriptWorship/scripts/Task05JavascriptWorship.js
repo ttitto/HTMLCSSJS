58bffb7262c26f538cc6fc4a806fb9e6de1fc9ed
ttitto <ttitto@users.noreply.github.com>
2014-06-19 15:50:23 +0300

    Tasks 04 and 05 solved

---
+++ SoftUni/HTMLSemanticsHW/HTMLSemanticsHW/Task05JavascriptWorship/scripts/Task05JavascriptWorship.js
@@ -0,0 +1,16 @@
+﻿document.addEventListener('DOMContentLoaded', domloaded, false);
+function domloaded() {
+    var c = document.getElementById("myCnvs");
+    var ctx = c.getContext("2d");
+    ctx.fillStyle = "#F1DA4E";
+    ctx.fillRect(50, 80, 134, 134);
+
+    ctx.fillStyle = "#000000";
+    ctx.font = "30px tahoma";
+    ctx.fillText("I love JavaScript", 10, 50);
+
+    ctx.fillStyle = "#323431";
+    ctx.font = "75px Geometr212 BkCn BT Heavy";
+    ctx.fontWeight = "bold";
+    ctx.fillText("JS", 90, 200);
+}
